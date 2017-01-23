faee88c677cf8ee0567e5aa7ae387d978a3533ec
TaHuC <TaHuC@hotmail.com>
2017-01-23 23:24:22 +0200

    Circle area found

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace CircleArea
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			double r = double.Parse(Console.ReadLine());
+			Console.WriteLine("{0:f12}", Math.PI * r * r);
+		}
+	}
+}
